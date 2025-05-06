90c7c5d2cb4e1ffd3e7f65c4fe282213e2151d39
rajeshkalle22 <rjshkalle@gmail.com>
2025-05-06 15:30:37 -0400

    Create sensor.cpp

---
+++ src/sensor.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void initSensor() {
+    std::cout << "Sensor initialized (mock)" << std::endl;
+}
+
+void readSensor() {
+    std::cout << "Temperature: 25°C, Gyro X: 0.01, Y: 0.02, Z: 0.00" << std::endl;
+}
